0e10abe52c6d5d81ef2b3d42db3864d4d9537371
hoomankhoshdel <hooman.khoshdel@gmail.com>
2023-08-06 03:36:56 +0330

    fix somethings in serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
-from blog.models import BlogPostModel
+from blog.models import BlogPostModel,Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['title',]
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source='author.email', read_only=True)
+    # category = CategorySerializer(many=True)
     class Meta:
             model = BlogPostModel
             fields = ['author','category','title','body','slug','background_image','created_date']
--- core/blog/mixins.py
+++ core/blog/mixins.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-from blog.models import BlogPostModel
+from blog.models import BlogPostModel,Category
 class FieldsMixin():
     def dispatch(self,request,*args, **kwargs):
         if request.user.is_superuser:
@@ -16,3 +16,9 @@ class AuthorAccesMixin():
             return super().dispatch(request,*args, **kwargs)
         else:
             raise Http404('you cant edit this post(you are not author of this post)')
+        
+# class CategoryTitleMixin():
+#     def dispatch(self,request,*args, **kwargs):
+#         article = get_object_or_404(BlogPostModel)
+#         print(article.category)
+#         return super().dispatch(request,*args, **kwargs)
